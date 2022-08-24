2df16be10c218a2948b7a9337cfdd5d3bcf20378
Julio Gutierrez <lcc.julio@gmail.com>
2022-08-24 14:19:53 -0500

    Update MovieService.java
    add missing @Service annotation, so spring can inject it

--- src/main/java/com/tutorial/movies/rest/springboot/movies/MovieService.java
+++ src/main/java/com/tutorial/movies/rest/springboot/movies/MovieService.java
@@ -1,5 +1,8 @@
 package com.tutorial.movies.rest.springboot.movies;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieService {
 
     public String generalInformation() {
